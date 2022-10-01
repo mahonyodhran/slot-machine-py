c327ce7951773235a44adfd996d28e837092d83f
Odhran <mahonyodhran@gmail.com>
2022-10-01 15:40:09 +0100

    created main.py and added basic deposit method

---
+++ main.py
@@ -0,0 +1,16 @@
+from curses.ascii import isdigit
+
+
+def deposit():
+    while True:
+        amount = input("How much would you like to deposit? €")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print('Amount must be greater than 0')
+        else:
+            print("Please enter a number.")
+    
+    return amount
