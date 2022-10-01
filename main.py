69d70190ceb00d2ad45b73d82af0d45f545c6c01
Odhran <mahonyodhran@gmail.com>
2022-10-01 16:56:33 +0100

    added end= to format printing

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row], "|")
+                print(column[row], end=" | ")
             else:
-                print(column[row])
+                print(column[row], end="")
 
 def deposit():
     while True:
