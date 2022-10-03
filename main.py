3c34cdbd199eeddc264d2cbbada17611bcd21245
Odhran <mahonyodhran@gmail.com>
2022-10-03 17:53:40 +0100

    added : to make question of playing look nicer

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def main():
     balance = deposit()
     while True:
         print(f"Current balance is €{balance}")
-        answer = input("Press enter to play (q to quit).")
+        answer = input("Press enter to play (q to quit) :")
         if answer == "q":
             break
         balance += spin(balance)
